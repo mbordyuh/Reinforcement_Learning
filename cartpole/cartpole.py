92f77d2d29bbbd104bff1f44805971794dbd2f3f
mbordyuh <mykola.bordyuh@prizer.com>
2020-05-25 12:27:05 -0400

    added cartpole folder

--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -11,12 +11,18 @@ def run_episode(env, parameters):
 
     while True:
         action = 0 if np.matmul(parameters, observation) < 0 else 1
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, _ = env.step(action)
         totalreward += reward
         if done:
             break
     return totalreward
 
+def draw(env, parameters):
+    observation = env.reset()
+    for _ in range(500):
+        env.render()
+        action = 0 if np.matmul(parameters, observation) < 0 else 1
+        observation, reward, done, _ = env.step(action)
 
 def random_search(env):
     bestreward = 0
@@ -30,7 +36,7 @@ def random_search(env):
             if reward == 200:
                 break
     print('Number of episodes to find a solution:', i)
-
+    draw(env, parameters)
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
